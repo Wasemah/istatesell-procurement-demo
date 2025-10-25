75c6c1f43b2290fd8378692e43e1319cdee586c5
Wasemah Binta Amran <wasemah2021@gmail.com>
2025-10-25 20:42:42 +0600

    Create __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,8 @@
+"""
+iStateSell Backend Package
+Real Estate Procurement System
+"""
+
+__version__ = "1.0.0"
+__author__ = "Wasemah"
+__description__ = "Real Estate Procurement and Quotation System"
